24851136a180569d7b11fe4176cad4af29f88bab
Huy Nguyen <huykst137@gmail.com>
2024-02-29 00:11:40 +0700

    feat: create form

--- src/components/Authenticated/AffiliateSponsor/index.tsx
+++ src/components/Authenticated/AffiliateSponsor/index.tsx
@@ -1,5 +1,168 @@
-import { Box } from '@mui/material';
+import { DevTool } from '@hookform/devtools';
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type FormType = {
+  companyName: string;
+  contactPersonName: string;
+  email: string;
+  phone: string;
+  website: string;
+  description: string;
+};
 
 export default function AffiliateSponsor() {
-  return <Box>AffiliateSponsor</Box>;
+  const { handleSubmit, register, control, formState } = useForm<FormType>({
+    mode: 'onTouched',
+    defaultValues: {
+      companyName: '',
+      contactPersonName: '',
+      email: '',
+      phone: '',
+      website: '',
+      description: '',
+    },
+  });
+
+  const { errors: formError } = formState;
+
+  const onSubmit: SubmitHandler<FormType> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{ marginY: 8 }}
+    >
+      <Typography
+        variant="h3"
+        noWrap
+        component="h3"
+        sx={{
+          display: 'flex',
+          fontWeight: 700,
+        }}
+      >
+        Affiliate Sponsor
+      </Typography>
+      <Box
+        component="form"
+        autoComplete="off"
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}
+      >
+        <Stack
+          direction="row"
+          spacing={2}
+        >
+          <FormControl
+            fullWidth
+            error={!!formError.companyName}
+          >
+            <FormLabel htmlFor="companyName">Company Name</FormLabel>
+            <TextField
+              {...register('companyName', {
+                required: 'The company name is required.',
+              })}
+              required
+              id="companyName"
+              error={!!formError.companyName}
+              aria-describedby="companyName-helper-text"
+            />
+            <FormHelperText id="companyName-helper-text">{formError.companyName?.message}</FormHelperText>
+          </FormControl>
+
+          <FormControl
+            fullWidth
+            error={!!formError.contactPersonName}
+          >
+            <FormLabel htmlFor="contactPersonName">Contact Person Name</FormLabel>
+            <TextField
+              {...register('contactPersonName', {
+                required: 'The contact person name is required.',
+              })}
+              id="contactPersonName"
+              error={!!formError.contactPersonName}
+              aria-describedby="contactPersonName-helper-text"
+            />
+            <FormHelperText id="contactPersonName-helper-text">{formError.contactPersonName?.message}</FormHelperText>
+          </FormControl>
+
+          <FormControl
+            fullWidth
+            error={!!formError.email}
+          >
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <TextField
+              {...register('email', {
+                required: 'The email is required.',
+              })}
+              id="email"
+              error={!!formError.email}
+              aria-describedby="email-helper-text"
+            />
+            <FormHelperText id="email-helper-text">{formError.email?.message}</FormHelperText>
+          </FormControl>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2}
+        >
+          <FormControl
+            fullWidth
+            error={!!formError.phone}
+          >
+            <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+            <TextField
+              {...register('phone', {
+                required: 'The phone number is required.',
+              })}
+              id="phone"
+              error={!!formError.phone}
+              aria-describedby="phone-helper-text"
+            />
+            <FormHelperText id="phone-helper-text">{formError.phone?.message}</FormHelperText>
+          </FormControl>
+          <FormControl
+            fullWidth
+            error={!!formError.website}
+          >
+            <FormLabel htmlFor="website">Website</FormLabel>
+            <TextField
+              {...register('website', {
+                required: 'The website is required.',
+              })}
+              id="website"
+              error={!!formError.website}
+              aria-describedby="website-helper-text"
+            />
+            <FormHelperText id="website-helper-text">{formError.website?.message}</FormHelperText>
+          </FormControl>
+        </Stack>
+        <FormControl fullWidth>
+          <FormLabel>Description</FormLabel>
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={false}
+          onClick={handleSubmit(onSubmit)}
+          sx={{ mt: 4 }}
+        >
+          Submit
+        </Button>
+        <DevTool control={control} /> {/* set up the dev tool */}
+      </Box>
+    </Container>
+  );
 }
