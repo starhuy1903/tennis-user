5a83fced9085534a9af381e600175ce7d5fa10c8
Huy Nguyen <huykst137@gmail.com>
2024-02-29 22:41:51 +0700

    refactor: move to base config

--- src/configurations/mock.ts
+++ src/configurations/mock.ts
@@ -2,8 +2,6 @@ import { baseConfigs } from './base';
 
 const configs = {
   apiUrl: 'http://localhost:8200/api',
-  oauthClientID: '57473750552-tvjo39ammmn2phv18nfncdtj7kkansru.apps.googleusercontent.com',
-  oauthClientSecret: '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',
 };
 
 export default Object.freeze({ ...baseConfigs, ...configs });
