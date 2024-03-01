593dc2ce73b81e9d9f55deb80ee9626aca65452b
starhuy1903 <huykst137@gmail.com>
2024-03-01 08:18:20 +0700

    feat: handle go to login

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,27 @@
 import { Box, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from 'store';
 
 import MyGroup from './MyGroup';
 import News from './News';
 
 export default function Home() {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAppSelector((state) => state.user);
+
+  const handleGoToAffiliateSponsor = () => {
+    if (isLoggedIn) {
+      navigate('/affiliate-sponsor');
+    } else {
+      navigate('/login');
+    }
+  };
+
   return (
     <Box>
       <Box>
         <Typography variant="h4">Quick Actions</Typography>
-        <Button onClick={() => navigate('/affiliate-sponsor')}>Affiliate sponsorship</Button>
+        <Button onClick={handleGoToAffiliateSponsor}>Affiliate sponsorship</Button>
       </Box>
       <Box>
         <Typography variant="h4">News</Typography>
