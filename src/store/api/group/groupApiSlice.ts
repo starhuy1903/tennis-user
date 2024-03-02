212f59619bd225e4ea3fe896da6118ba67b0317b
Huy Nguyen <huykst137@gmail.com>
2024-03-02 17:43:42 +0700

    feat: invitation feature

---
+++ src/store/api/group/groupApiSlice.ts
@@ -0,0 +1,17 @@
+import { InvitationPayload } from 'types/group';
+
+import { apiWithToastSlice } from '../baseApiSlice';
+
+const groupApiToastSlice = apiWithToastSlice.injectEndpoints({
+  endpoints: (build) => ({
+    addMember: build.mutation<void, InvitationPayload>({
+      query: (body) => ({
+        url: 'groups/invite',
+        method: 'POST',
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { useAddMemberMutation } = groupApiToastSlice;
