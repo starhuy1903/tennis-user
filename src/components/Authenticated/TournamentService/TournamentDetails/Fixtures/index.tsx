fde46322340cd53e8232ad62422bd200314f692e
starhuy1903 <huykst137@gmail.com>
2024-04-11 09:55:08 +0700

    feat: create fixture layout

--- src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/index.tsx
+++ src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/index.tsx
@@ -1,3 +1,21 @@
+import { Box } from '@mui/material';
+import { useState } from 'react';
+
+import MatchesList from './MatchesList';
+import SetupFixture from './SetupFixture';
+
 export default function Fixtures() {
-  return <div>Fixtures</div>;
+  const [matchesData, setMatchesData] = useState([]);
+
+  // TODO: get participants from the tournament
+
+  return (
+    <Box
+      mt={4}
+      mb={4}
+    >
+      <SetupFixture />
+      {matchesData.length > 0 && <MatchesList matches={matchesData} />}
+    </Box>
+  );
 }
