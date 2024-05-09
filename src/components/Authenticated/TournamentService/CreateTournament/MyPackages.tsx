02ed563ea5c12c28acb47c6e734b32ae0ba7fac1
Do Minh Tri <mitidevus@gmail.com>
2024-05-09 14:01:37 +0700

    chore: delete old component

--- src/components/Authenticated/TournamentService/CreateTournament/MyPackages.tsx
+++ src/components/Authenticated/TournamentService/CreateTournament/MyPackages.tsx
@@ -18,6 +18,7 @@ export default function MyPackages({ packagesData, onChooseMyPackage }: MyPackag
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          height: 500,
         }}
       >
         <NoData
