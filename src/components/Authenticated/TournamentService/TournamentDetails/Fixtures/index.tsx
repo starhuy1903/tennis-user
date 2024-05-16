f356101b9d5947a002225a176d14a6d7f8b47dda
starhuy1903 <huykst137@gmail.com>
2024-05-16 18:34:27 +0700

    refactor: render UI follow tournament phase

--- src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/index.tsx
+++ src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/index.tsx
@@ -1,6 +1,6 @@
 import { Alert, Box, Button } from '@mui/material';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'store';
 
 import CenterLoading from 'components/Common/CenterLoading';
@@ -8,7 +8,7 @@ import GroupPlayoffFixture from 'components/Common/Fixtures/GroupPlayoffFixture'
 import KnockoutFixtures from 'components/Common/Fixtures/KnockoutFixture';
 import { RoundRobinFixture } from 'components/Common/Fixtures/RoundRobinFixture';
 import { ModalKey } from 'constants/modal';
-import { TournamentFormat } from 'constants/tournament';
+import { TournamentFormat, TournamentPhase } from 'constants/tournament';
 import { FixtureStatus } from 'constants/tournament-fixtures';
 import { useLazyGetTournamentFixtureQuery } from 'store/api/tournament/shared/fixture';
 import { showModal } from 'store/slice/modalSlice';
@@ -50,10 +50,56 @@ export default function Fixtures() {
 
   if (fetchingFixture) return <CenterLoading />;
 
-  if (
-    !isCreator &&
-    (!fixture || (fixture?.status && [FixtureStatus.NEW, FixtureStatus.DRAFT].includes(fixture.status)))
-  )
+  if (isCreator) {
+    if (tournamentData.phase === TournamentPhase.NEW) {
+      return (
+        <Box mt={4}>
+          <Alert severity="info">
+            You need to publish the tournament first in the{' '}
+            <Link to={`/tournaments/${tournamentData.id}/info`}>Information</Link> tab.
+          </Alert>
+        </Box>
+      );
+    } else if (tournamentData.phase === TournamentPhase.PUBLISHED) {
+      return (
+        <Box mt={4}>
+          <Alert severity="info">
+            You need to finalize applicant list in the{' '}
+            <Link to={`/tournaments/${tournamentData.id}/participants`}>Participants</Link> tab.
+          </Alert>
+        </Box>
+      );
+    } else {
+      const shouldRenderSetupForm =
+        tournamentData.phase === TournamentPhase.FINALIZED_APPLICANTS && fixture?.status === FixtureStatus.NEW;
+
+      return (
+        <Box mt={4}>
+          {shouldRenderSetupForm && <SetupFixture />}
+          {fixture && (
+            <>
+              {fixture.format === TournamentFormat.KNOCKOUT && <KnockoutFixtures rounds={fixture.knockoutRounds!} />}
+              {fixture.format === TournamentFormat.ROUND_ROBIN && (
+                <RoundRobinFixture rounds={fixture.roundRobinRounds!} />
+              )}
+              {fixture.format === TournamentFormat.GROUP_PLAYOFF && <GroupPlayoffFixture fixture={fixture} />}
+            </>
+          )}
+          {/* Create match button */}
+          <Box>
+            <Button
+              variant="text"
+              onClick={handleAddMatch}
+            >
+              Add match
+            </Button>
+          </Box>
+        </Box>
+      );
+    }
+  }
+
+  if (!fixture || (fixture.status && [FixtureStatus.NEW, FixtureStatus.DRAFT].includes(fixture.status)))
     return (
       <Box mt={4}>
         <Alert severity="info">In the process of generating fixtures. Please wait for the organizers to publish.</Alert>
@@ -62,20 +108,9 @@ export default function Fixtures() {
 
   return (
     <Box mt={4}>
-      {isCreator && fixture?.status === FixtureStatus.NEW && <SetupFixture />}
       {fixture?.format === TournamentFormat.KNOCKOUT && <KnockoutFixtures rounds={fixture.knockoutRounds!} />}
       {fixture?.format === TournamentFormat.ROUND_ROBIN && <RoundRobinFixture rounds={fixture.roundRobinRounds!} />}
       {fixture?.format === TournamentFormat.GROUP_PLAYOFF && <GroupPlayoffFixture fixture={fixture} />}
-
-      {/* Create match button */}
-      <Box>
-        <Button
-          variant="text"
-          onClick={handleAddMatch}
-        >
-          Add match
-        </Button>
-      </Box>
     </Box>
   );
 }
