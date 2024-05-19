0dbd887fd6937a10fcab0b07e30b112a5d25cc50
Do Minh Tri <mitidevus@gmail.com>
2024-05-19 18:55:35 +0700

    fix: add navigation button for slide

--- src/components/Authenticated/TournamentService/TournamentList/TournamentItem.tsx
+++ src/components/Authenticated/TournamentService/TournamentList/TournamentItem.tsx
@@ -29,7 +29,7 @@ export default function TournamentItem({
       sx={{
         my: 2,
         position: 'relative',
-        width: '320px',
+        width: '300px',
       }}
     >
       <CardActionArea>
