c0bb6ca45c8c972e2f15821b8533a030117c49b3
Do Minh Tri <mitidevus@gmail.com>
2024-06-23 02:21:50 +0700

    chore: style signup ui

--- src/components/Unauthenticated/Signup/index.tsx
+++ src/components/Unauthenticated/Signup/index.tsx
@@ -9,14 +9,16 @@ import {
   Stack,
   TextField,
   Typography,
+  useTheme,
 } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
+import * as EmailValidator from 'email-validator';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { Breadcrumbs } from 'components/Common/Breadcrumb';
+import { APP_BACKGROUND_URL } from 'constants/app';
 import { Gender, GenderOptions } from 'constants/tournament';
 import { useSignupMutation } from 'store/api/userApiSlice';
 import { SignupPayload } from 'types/user';
@@ -28,6 +30,7 @@ type FormType = SignupPayload & {
 
 export default function Signup() {
   const navigate = useNavigate();
+  const theme = useTheme();
 
   const [requestSignup, { isLoading: isSubmitting }] = useSignupMutation();
 
@@ -67,45 +70,75 @@ export default function Signup() {
   };
 
   return (
-    <Box>
-      <Breadcrumbs />
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        minHeight: `calc(100dvh - ${theme.layout.headerHeight})`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Box
+        component="img"
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          position: 'absolute',
+          width: '100vw',
           height: '100%',
+          filter: 'brightness(0.6)',
+        }}
+        src={APP_BACKGROUND_URL}
+        alt="signup"
+      />
+
+      <Box
+        component="form"
+        autoComplete="off"
+        sx={{
+          position: 'relative',
+          minWidth: 400,
+          maxWidth: 550,
+          width: '100%',
+          px: 6,
+          py: 4,
+          borderRadius: 2,
+          backgroundColor: 'white',
+          boxShadow: 2,
         }}
       >
-        <Box
-          component="form"
-          autoComplete="off"
+        <Typography
+          variant="h5"
+          gutterBottom
           sx={{
-            px: 8,
-            py: 4,
-            borderRadius: 2,
-            backgroundColor: 'white',
-            boxShadow: 2,
+            fontWeight: 500,
+            textAlign: 'center',
           }}
         >
-          <Typography
-            variant="h4"
-            gutterBottom
-            sx={{
-              fontWeight: 600,
-              textAlign: 'center',
-            }}
-          >
-            Create an account
-          </Typography>
+          Create an account
+        </Typography>
 
-          <Box
-            sx={{
-              mt: 2,
-            }}
+        <Typography
+          variant="subtitle2"
+          sx={{
+            color: 'gray',
+            textAlign: 'center',
+          }}
+        >
+          Sign up to get started!
+        </Typography>
+
+        <Box
+          sx={{
+            mt: 2,
+          }}
+        >
+          <Stack
+            direction="column"
+            spacing={2}
           >
             <Stack
-              direction="column"
+              direction="row"
               spacing={2}
             >
               <FormControl
@@ -134,83 +167,6 @@ export default function Signup() {
                 <FormHelperText id="name-helper-text">{formError.name?.message}</FormHelperText>
               </FormControl>
 
-              <Stack
-                direction="row"
-                spacing={2}
-              >
-                <Controller
-                  control={control}
-                  name="gender"
-                  defaultValue={Gender.ANY}
-                  rules={{ required: 'Please select a gender.' }}
-                  render={({ field: { onChange, value } }) => (
-                    <FormControl
-                      fullWidth
-                      error={!!formError.gender}
-                    >
-                      <FormLabel htmlFor="gender">Gender</FormLabel>
-                      <Select
-                        value={value}
-                        id="gender"
-                        onChange={onChange}
-                        aria-describedby="gender-helper-text"
-                        size="small"
-                        sx={{
-                          backgroundColor: 'white',
-                        }}
-                        disabled={isSubmitting}
-                      >
-                        {Object.entries(GenderOptions).map(([genderKey, genderValue], index) => (
-                          <MenuItem
-                            key={index}
-                            value={genderKey}
-                          >
-                            {genderValue}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                      <FormHelperText id="gender-helper-text">{formError.gender?.message}</FormHelperText>
-                    </FormControl>
-                  )}
-                />
-
-                <Controller
-                  control={control}
-                  name="dob"
-                  rules={{
-                    required: 'Date of birth is required!',
-                  }}
-                  render={({ field: { onChange } }) => (
-                    <FormControl
-                      fullWidth
-                      error={!!formError.dob}
-                    >
-                      <FormLabel htmlFor="dob">Date of birth</FormLabel>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          disabled={isSubmitting}
-                          onChange={(date) => {
-                            onChange(date?.toISOString());
-                          }}
-                          defaultValue={dayjs(getValues('dob'))}
-                          format="DD/MM/YYYY"
-                          disableFuture
-                          slotProps={{
-                            textField: {
-                              size: 'small',
-                              sx: {
-                                backgroundColor: 'white',
-                              },
-                            },
-                          }}
-                        />
-                      </LocalizationProvider>
-                      <FormHelperText id="dob-helper-text">{formError.dob?.message}</FormHelperText>
-                    </FormControl>
-                  )}
-                />
-              </Stack>
-
               <FormControl
                 fullWidth
                 error={!!formError.phoneNumber}
@@ -232,123 +188,197 @@ export default function Signup() {
                 />
                 <FormHelperText id="phoneNumber-helper-text">{formError.phoneNumber?.message}</FormHelperText>
               </FormControl>
-
-              <FormControl
-                fullWidth
-                error={!!formError.email}
-              >
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <TextField
-                  {...register('email', {
-                    required: 'Email is required!',
-                    pattern: {
-                      value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-                      message: 'Invalid email address!',
-                    },
-                  })}
-                  id="email"
-                  placeholder="Enter your email"
-                  error={!!formError.email}
-                  aria-describedby="email-helper-text"
-                  size="small"
-                  sx={{
-                    backgroundColor: 'white',
-                  }}
-                  disabled={isSubmitting}
-                />
-                <FormHelperText id="email-helper-text">{formError.email?.message}</FormHelperText>
-              </FormControl>
-
-              <FormControl
-                fullWidth
-                error={!!formError.password}
-              >
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <TextField
-                  {...register('password', {
-                    required: 'Password is required!',
-                    minLength: {
-                      value: 6,
-                      message: 'Password must be at least 6 characters!',
-                    },
-                    maxLength: {
-                      value: 20,
-                      message: 'Password must be at most 20 characters!',
-                    },
-                    validate: {
-                      hasNumber: (value) => /\d/.test(value) || 'Password must contain a number!',
-                      hasUppercase: (value) => /[A-Z]/.test(value) || 'Password must contain an uppercase letter!',
-                      hasLowercase: (value) => /[a-z]/.test(value) || 'Password must contain a lowercase letter!',
-                    },
-                  })}
-                  id="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  error={!!formError.password}
-                  aria-describedby="password-helper-text"
-                  size="small"
-                  sx={{
-                    backgroundColor: 'white',
-                  }}
-                  disabled={isSubmitting}
-                />
-                <FormHelperText id="password-helper-text">{formError.password?.message}</FormHelperText>
-              </FormControl>
-
-              <FormControl
-                fullWidth
-                error={!!formError.confirmPassword}
-              >
-                <FormLabel htmlFor="confirmPassword">Confirm password</FormLabel>
-                <TextField
-                  {...register('confirmPassword', {
-                    required: 'Confirm password is required!',
-                    validate: (value) => value === formValue.password || `Password confirmation doesn't match!`,
-                  })}
-                  id="confirmPassword"
-                  type="password"
-                  placeholder="Enter your confirm password"
-                  error={!!formError.confirmPassword}
-                  aria-describedby="confirmPassword-helper-text"
-                  size="small"
-                  sx={{
-                    backgroundColor: 'white',
-                  }}
-                  disabled={isSubmitting}
-                />
-                <FormHelperText id="confirmPassword-helper-text">{formError.confirmPassword?.message}</FormHelperText>
-              </FormControl>
             </Stack>
 
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={isSubmitting}
-              onClick={handleSubmit(onSubmit)}
-              sx={{ mt: 4, color: 'white', width: '100%' }}
+            <Stack
+              direction="row"
+              spacing={2}
             >
-              {isSubmitting ? 'Loading ...' : 'Get started'}
-            </Button>
+              <Controller
+                control={control}
+                name="dob"
+                rules={{
+                  required: 'Date of birth is required!',
+                }}
+                render={({ field: { onChange } }) => (
+                  <FormControl
+                    fullWidth
+                    error={!!formError.dob}
+                  >
+                    <FormLabel htmlFor="dob">Date of birth</FormLabel>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker
+                        disabled={isSubmitting}
+                        onChange={(date) => {
+                          onChange(date?.toISOString());
+                        }}
+                        defaultValue={dayjs(getValues('dob'))}
+                        format="DD/MM/YYYY"
+                        disableFuture
+                        slotProps={{
+                          textField: {
+                            size: 'small',
+                            sx: {
+                              backgroundColor: 'white',
+                            },
+                          },
+                        }}
+                      />
+                    </LocalizationProvider>
+                    <FormHelperText id="dob-helper-text">{formError.dob?.message}</FormHelperText>
+                  </FormControl>
+                )}
+              />
 
-            <Typography
-              variant="subtitle2"
-              gutterBottom
-              sx={{
-                'mt': 2,
-                'textAlign': 'center',
-                '& a': {
-                  'textDecoration': 'none',
-                  'color': (theme) => theme.palette.primary.main,
-                  '&:hover': {
-                    textDecoration: 'underline',
-                  },
-                },
-              }}
+              <Controller
+                control={control}
+                name="gender"
+                defaultValue={Gender.ANY}
+                rules={{ required: 'Please select a gender.' }}
+                render={({ field: { onChange, value } }) => (
+                  <FormControl
+                    fullWidth
+                    error={!!formError.gender}
+                  >
+                    <FormLabel htmlFor="gender">Gender</FormLabel>
+                    <Select
+                      value={value}
+                      id="gender"
+                      onChange={onChange}
+                      aria-describedby="gender-helper-text"
+                      size="small"
+                      sx={{
+                        backgroundColor: 'white',
+                      }}
+                      disabled={isSubmitting}
+                    >
+                      {Object.entries(GenderOptions).map(([genderKey, genderValue], index) => (
+                        <MenuItem
+                          key={index}
+                          value={genderKey}
+                        >
+                          {genderValue}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText id="gender-helper-text">{formError.gender?.message}</FormHelperText>
+                  </FormControl>
+                )}
+              />
+            </Stack>
+
+            <FormControl
+              fullWidth
+              error={!!formError.email}
             >
-              Already have an account? <Link to="/login">Log in</Link>
-            </Typography>
-          </Box>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                {...register('email', {
+                  required: 'Email is required!',
+                  validate: (value) => EmailValidator.validate(value) || 'Invalid email address!',
+                })}
+                id="email"
+                placeholder="Enter your email"
+                error={!!formError.email}
+                aria-describedby="email-helper-text"
+                size="small"
+                sx={{
+                  backgroundColor: 'white',
+                }}
+                disabled={isSubmitting}
+              />
+              <FormHelperText id="email-helper-text">{formError.email?.message}</FormHelperText>
+            </FormControl>
+
+            <FormControl
+              fullWidth
+              error={!!formError.password}
+            >
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <TextField
+                {...register('password', {
+                  required: 'Password is required!',
+                  minLength: {
+                    value: 6,
+                    message: 'Password must be at least 6 characters!',
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: 'Password must be at most 20 characters!',
+                  },
+                  validate: {
+                    hasNumber: (value) => /\d/.test(value) || 'Password must contain a number!',
+                    hasUppercase: (value) => /[A-Z]/.test(value) || 'Password must contain an uppercase letter!',
+                    hasLowercase: (value) => /[a-z]/.test(value) || 'Password must contain a lowercase letter!',
+                  },
+                })}
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                error={!!formError.password}
+                aria-describedby="password-helper-text"
+                size="small"
+                sx={{
+                  backgroundColor: 'white',
+                }}
+                disabled={isSubmitting}
+              />
+              <FormHelperText id="password-helper-text">{formError.password?.message}</FormHelperText>
+            </FormControl>
+
+            <FormControl
+              fullWidth
+              error={!!formError.confirmPassword}
+            >
+              <FormLabel htmlFor="confirmPassword">Confirm password</FormLabel>
+              <TextField
+                {...register('confirmPassword', {
+                  required: 'Confirm password is required!',
+                  validate: (value) => value === formValue.password || `Password confirmation doesn't match!`,
+                })}
+                id="confirmPassword"
+                type="password"
+                placeholder="Enter your confirm password"
+                error={!!formError.confirmPassword}
+                aria-describedby="confirmPassword-helper-text"
+                size="small"
+                sx={{
+                  backgroundColor: 'white',
+                }}
+                disabled={isSubmitting}
+              />
+              <FormHelperText id="confirmPassword-helper-text">{formError.confirmPassword?.message}</FormHelperText>
+            </FormControl>
+          </Stack>
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            onClick={handleSubmit(onSubmit)}
+            sx={{ mt: 4, color: 'white', width: '100%' }}
+          >
+            {isSubmitting ? 'Loading ...' : 'Get started'}
+          </Button>
+
+          <Typography
+            variant="subtitle2"
+            gutterBottom
+            sx={{
+              'mt': 2,
+              'textAlign': 'center',
+              '& a': {
+                'textDecoration': 'none',
+                'color': 'primary.main',
+                '&:hover': {
+                  textDecoration: 'underline',
+                },
+              },
+            }}
+          >
+            Already have an account? <Link to="/login">Log in</Link>
+          </Typography>
         </Box>
       </Box>
     </Box>
