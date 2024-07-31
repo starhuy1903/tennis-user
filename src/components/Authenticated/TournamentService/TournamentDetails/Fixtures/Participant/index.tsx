cc6c24e63eea7446d850485f72d8563d424fdc90
Huy Nguyen <huykst137@gmail.com>
2024-08-01 00:14:30 +0700

    chore: clean up code

--- src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/Participant/index.tsx
+++ src/components/Authenticated/TournamentService/TournamentDetails/Fixtures/Participant/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box } from '@mui/material';
+import { Alert } from '@mui/material';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from 'store';
